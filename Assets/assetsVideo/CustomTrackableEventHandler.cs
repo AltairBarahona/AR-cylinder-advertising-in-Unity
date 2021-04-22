8a4e85e5b67310c3d9549856755f96b7e75cb6eb
AltairBarahona <altair.profesional@gmail.com>
2021-04-22 15:38:59 -0500

    Proyect Finished

---
+++ Assets/assetsVideo/CustomTrackableEventHandler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+
+    public bool isVideoEnabled;
+    public string url;
+
+
+    [Space]
+
+    public GameObject videoGO;
+    private Button interactionBtn;
+    
+    private VideoPlayer videoPlayer;
+    private RawImage rawImage;
+
+    private void Awake(){
+        interactionBtn=videoGO.GetComponent<Button>();
+        videoPlayer=videoGO.GetComponent<VideoPlayer>();
+        rawImage=videoGO.GetComponent<RawImage>();
+
+    }
+    protected override void Start()
+    {
+        
+        base.Start();
+        //generar interacción
+
+        interactionBtn.onClick.AddListener(OpenUrl2);
+        StartCoroutine(PrepareVideo());
+
+    }
+
+    private void OpenUrl2(){
+        Application.OpenURL(url);
+    }
+
+    private IEnumerator PrepareVideo()
+    {
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+
+        rawImage.texture = videoPlayer.texture;
+
+        isVideoEnabled = true;
+    }
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if(isVideoEnabled){
+            videoPlayer.Play();
+        }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if(isVideoEnabled){
+            videoPlayer.Pause();
+        }
+    }
+
+    private void OpenUrl(){
+        Application.OpenURL(url);
+    }
+}
